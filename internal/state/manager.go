1dc36d58792fd0c40c4de3fae1efc8693e8f00f7
Robson Cruz <ghost0092@gmail.com>
2024-01-29 14:23:04 -0300

    feat(internal/state): add basic state manager
    This basic structure will serve as an state management engine, where changes are saved into the
    struct and then written into a JSON file for simplicity

---
+++ internal/state/manager.go
@@ -0,0 +1,30 @@
+package state
+
+import (
+	"encoding/json"
+	"os"
+	"time"
+)
+
+type FlowmodoroState struct {
+	SessiionActive bool      `json:"session_active"`
+	StartTime      time.Time `json:"start_time"`
+}
+
+func SaveState(state FlowmodoroState, filepath string) error {
+	data, err := json.Marshal(state)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filepath, data, 0644)
+}
+
+func LoadState(filepath string) (FlowmodoroState, error) {
+	var state FlowmodoroState
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		return FlowmodoroState{}, err
+	}
+	err = json.Unmarshal(data, &state)
+	return state, err
+}
