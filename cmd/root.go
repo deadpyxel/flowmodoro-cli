3fed8e09e5fb75b3ab1fcb79e9fc019325ae32f5
Robson Cruz <ghost0092@gmail.com>
2024-02-07 12:08:06 -0300

    feat(cmd/root): add viper configuration settings
    This should allow us to use a YAML configuration file to customize some
    of the behaviour of the tool. For now only the path to save the state is
    set, but probably extra settings can be done.

--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,10 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var cfgFile string
 
 var rootCmd = &cobra.Command{
 	Use:   "flowmodoro",
@@ -22,5 +25,36 @@ func Execute() {
 }
 
 func init() {
+	cobra.OnInitialize(initConfig)
+	// add config flag
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.config/flowmodoro-cli/config.yaml)")
+}
 
+func initConfig() {
+
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search for config file in XDG config directory with name "flowmodoro-cli" (without extension).
+		viper.AddConfigPath(home + "/.config/flowmodoro-cli")
+		viper.SetConfigType("yaml")
+		viper.SetConfigName("config.yaml")
+	}
+
+	// Set default value for statePath. Will use your HOME if not set.
+	home, err := os.UserHomeDir()
+	cobra.CheckErr(err)
+	viper.SetDefault("statePath", home+"/state.json")
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
 }
